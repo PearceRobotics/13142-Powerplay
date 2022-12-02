3ee6acb8915f37338a03154dcc0444dfe1e7c8b9
YedaniR <113949146+YedaniR@users.noreply.github.com>
2022-12-01 20:05:12 -0600

    Update Auton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -1,1 +1,35 @@
+package org.firstinspires.ftc.teamcode;
 
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import  com.qualcomm.robotcore.hardware.TouchSensor;
+public class Auton extends motorsetup
+    {
+    double clicksPerInch = 8.1;
+
+    int leftFrontPos = leftFrontDrive.getCurrentPosition();
+    int rightFrontPos = rightFrontDrive.getCurrentPosition();
+    int leftBackPos = leftBackDrive.getCurrentPosition();
+    int rightBackPos = rightBackDrive.getCurrentPosition();
+
+        public void forward(double howFar, double howFast)
+        {
+            leftFrontPos += howFar * clicksPerInch;
+            rightFrontPos += howFar * clicksPerInch;
+            leftBackPos += howFar * clicksPerInch;
+            rightBackPos += howFar * clicksPerInch;
+
+            leftFrontDrive.setPower(howFast);
+            rightFrontDrive.setPower(howFar);
+            leftBackDrive.setPower(howFar);
+            rightBackDrive.setPower(howFar);
+
+            leftFrontDrive.setTargetPosition(leftFrontPos);
+            rightFrontDrive.setTargetPosition(rightFrontPos);
+            leftBackDrive.setTargetPosition(leftBackPos);
+            rightBackDrive.setTargetPosition(rightBackPos);
+        }
+
+    }
